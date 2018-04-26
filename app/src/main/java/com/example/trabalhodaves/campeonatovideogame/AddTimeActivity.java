c471b37b4b441e0122985c5609824592ea5cca84
Felipe Ferraz <felipe.ferraz.developer@gmail.com>
2018-04-26 17:50:37 -0300

    Inserindo algumas coisas

--- app/src/main/java/com/example/trabalhodaves/campeonatovideogame/AddTimeActivity.java
+++ app/src/main/java/com/example/trabalhodaves/campeonatovideogame/AddTimeActivity.java
@@ -1,13 +1,61 @@
 package com.example.trabalhodaves.campeonatovideogame;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.example.trabalhodaves.campeonatovideogame.model.Jogador;
+
+import java.util.UUID;
+
+import io.realm.Realm;
 
 public class AddTimeActivity extends AppCompatActivity {
+
+    ListView jogadores;
+    Button btnGravar, btnAddJogador;
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_time);
+        binding();
+
+        btnAddJogador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText editText = new EditText(AddTimeActivity.this);
+                AlertDialog alert = new AlertDialog.Builder(AddTimeActivity.this)
+                        .setTitle("Adicionar Jogador")
+                        .setView(editText)
+                        .setPositiveButton("Adiciona", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        realm.executeTransactionAsync(new Realm.Transaction() {
+                            @Override
+                            public void execute(Realm realm) {
+                                realm.createObject(Jogador.class, UUID.randomUUID().toString())
+                                        .setNome(String.valueOf(editText.getText()));
+                            }
+                        });
+                    }
+                })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+            }
+        });
+    }
+
+    private void binding() {
+        jogadores = findViewById(R.id.listViewJogadores);
+        btnGravar = findViewById(R.id.btnGravarTime);
+        btnAddJogador = findViewById(R.id.btnAdicionaJogadorTime);
+        realm = Realm.getDefaultInstance();
     }
 }
--- app/src/main/java/com/example/trabalhodaves/campeonatovideogame/model/Jogador.java
+++ app/src/main/java/com/example/trabalhodaves/campeonatovideogame/model/Jogador.java
@@ -1,9 +1,47 @@
 package com.example.trabalhodaves.campeonatovideogame.model;
 
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
 
 public class Jogador extends RealmObject {
+    @Required
+    @PrimaryKey
+    private String id;
+    @Required
     private String nome;
     private int golsMarcados;
     private Time time;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getGolsMarcados() {
+        return golsMarcados;
+    }
+
+    public void setGolsMarcados(int golsMarcados) {
+        this.golsMarcados = golsMarcados;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
 }
