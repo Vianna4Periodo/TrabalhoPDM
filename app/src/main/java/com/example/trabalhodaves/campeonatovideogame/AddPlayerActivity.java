b81f20402eaabdfb9b3097e2fc2b89d79d7bff48
Felipe Ferraz <felipe.ferraz.developer@gmail.com>
2018-04-27 15:04:44 -0300

    inserindo coisas

--- app/src/main/java/com/example/trabalhodaves/campeonatovideogame/AddPlayerActivity.java
+++ app/src/main/java/com/example/trabalhodaves/campeonatovideogame/AddPlayerActivity.java
@@ -2,12 +2,56 @@ package com.example.trabalhodaves.campeonatovideogame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.trabalhodaves.campeonatovideogame.model.Player;
+
+import java.util.List;
+import java.util.UUID;
+
+import io.realm.Realm;
 
 public class AddPlayerActivity extends AppCompatActivity {
 
+    EditText edtNomePlayer, edtIdadePlayer;
+    Button btnGravarPlayer;
+    Realm realm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_player);
+
+        binding();
+
+        realm.executeTransaction(new Realm.Transaction(){
+            @Override
+            public void execute(Realm realm){
+                List<Player> players = realm.where(Player.class).findAll();
+                int i = players.size();
+            }
+        });
+        btnGravarPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                realm.executeTransaction(new Realm.Transaction(){
+                    @Override
+                    public void execute(Realm realm) {
+                        Player player = realm.createObject(Player.class, UUID.randomUUID());
+                        player.setNome(edtNomePlayer.getText().toString());
+                        player.setIdade(Integer.parseInt(edtIdadePlayer.getText().toString()));
+                        player.setPontuacao(0);
+                    }
+                });
+            }
+        });
+    }
+
+    private void binding() {
+        edtIdadePlayer = findViewById(R.id.edtIdadePlayer);
+        edtNomePlayer = findViewById(R.id.edtNomePlayer);
+        btnGravarPlayer = findViewById(R.id.btnGravarPlayer);
+        realm = Realm.getDefaultInstance();
     }
 }
--- app/src/main/java/com/example/trabalhodaves/campeonatovideogame/model/Player.java
+++ app/src/main/java/com/example/trabalhodaves/campeonatovideogame/model/Player.java
@@ -1,9 +1,51 @@
 package com.example.trabalhodaves.campeonatovideogame.model;
 
+import java.util.UUID;
+
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
 
 public class Player extends RealmObject {
+
+    @PrimaryKey
+    @Required
+    private UUID id;
+
+    @Required
     private String nome;
     private int idade;
     private int pontuacao;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
 }
